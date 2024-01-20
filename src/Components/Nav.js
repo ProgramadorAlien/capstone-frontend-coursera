0628d360adde2c3358f10fa99f65039bc42db8ad
AlienMartinezDev <110763299+ProgramadorAlien@users.noreply.github.com>
2024-01-20 18:49:57 -0500

    coding css to navbar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import Logo from '../Images/Logo.png';
 
 const Nav = () => {
-    return (  
-    <nav>           
-        <h1>Home Page</h1>
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+    }
+
+    return (
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
         <img src={Logo} alt="Description"/>
-        
-        <ul>
-            <li><a href="#" >Home</a></li>
-            <li><a href="#" >About</a></li>
-            <li><a href="#" >Menu</a></li>
-            <li><a href="#" >Reservation</a></li>
-            <li><a href="#" >Order Online</a></li>
-            <li><a href="#" >Login</a></li>
+        {/*Mobil Navbar*/}
+    <div className='menu-icon' onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+    </div>
+        <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+            <li>
+                <a href="#" >Home</a>
+            </li>
+            <li>
+                <a href="#" >About</a>
+            </li>
+            <li>
+                <a href="#" >Menu</a>
+            </li>
+            <li>
+                <a href="#" >Reservation</a>
+            </li>
+            <li>
+                <a href="#" >Order Online</a>
+            </li>
+            <li>
+                <a href="#" >Login</a>
+            </li>
         </ul>
     </nav>
-)}
+);}
 
 export default Nav;
