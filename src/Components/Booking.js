028dfd88a63a63167512d1077da8011c7e6244d9
AlienMartinezDev <110763299+ProgramadorAlien@users.noreply.github.com>
2024-01-28 14:05:30 -0500

    Create Booking Component

---
+++ src/Components/Booking.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Nav from './Nav'
+
+const Booking = () => {
+    return (
+        <>
+            <Nav />
+            <div>
+                <h1>Booking</h1>
+                <h1>Booking</h1>
+                <h1>Booking</h1>
+                <h1>Booking</h1>
+            </div>
+        </>
+    );
+}
+
+export default Booking;
