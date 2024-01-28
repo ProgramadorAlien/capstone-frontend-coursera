f3aeab5fe63144b66b7912d92c8b31ca37dfb66a
AlienMartinezDev <110763299+ProgramadorAlien@users.noreply.github.com>
2024-01-28 14:08:21 -0500

    Crate Home Component

---
+++ src/Components/Home.js
@@ -0,0 +1,23 @@
+import '../App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Menu from './Menu';
+import Footer from './Footer';
+import { Routes, Route, Link } from 'react-router-dom';
+import Booking from './Booking';
+
+const Home = () => {
+    return (
+        <>
+
+            <Nav />
+            <Header />
+            <Menu />
+            <Footer />
+
+           
+        </>
+    );
+}
+
+export default Home;
